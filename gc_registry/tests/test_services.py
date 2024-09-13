e7f50dbd5173e08aec2d154d6368d7f471e56135
Clem <clem@futureenergy.associates>
2024-09-13 08:47:18 +0100

    Testing imports

--- gc_registry/tests/test_services.py
+++ gc_registry/tests/test_services.py
@@ -1,3 +1,11 @@
-def test_fixtures_work(fake_db_wind_device, fake_db_solar_device):
+import pytest
+from sqlmodel import Session
+
+from gc_registry.device.models import Device
+
+
+def test_fixtures_work(
+    db_session: Session, fake_db_wind_device: Device, fake_db_solar_device: Device
+):
     assert fake_db_wind_device.device_name == "fake_wind_device"
     assert fake_db_solar_device.device_name == "fake_solar_device"
