b378085400d8ab1bc94471ae3f60c83c33bef2b9
Clem <clem@futureenergy.associates>
2024-09-13 13:55:29 +0100

    Now what?

--- gc_registry/tests/test_services.py
+++ gc_registry/tests/test_services.py
@@ -1,11 +1,13 @@
-# from sqlmodel import Session
+from sqlmodel import Session
 
-# from gc_registry.device.models import Device
+from gc_registry.device.models import Device
 
 
-# def test_fixtures_work(
-#     db_session: Session, fake_db_wind_device: Device, fake_db_solar_device: Device
-# ):
-#     assert 1 == 2
+def test_fixtures_work(
+    db_session: Session, fake_db_wind_device: Device, fake_db_solar_device: Device
+):
+    assert 1 == 2
+
+
 #     assert fake_db_wind_device.device_name == "fake_wind_device"
 #     assert fake_db_solar_device.device_name == "fake_solar_device"
