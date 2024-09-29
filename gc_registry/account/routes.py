e0e64602d382acaa063b09c3f2abefb042f86a6a
Connor Galbraith <33663738+CAGalbraith@users.noreply.github.com>
2024-09-29 22:00:06 +0100

    Get CQRS working with `Account` CRUD routes

--- gc_registry/account/routes.py
+++ gc_registry/account/routes.py
@@ -4,26 +4,23 @@ from sqlmodel import Session
 from gc_registry import utils
 from gc_registry.account import models
 from gc_registry.authentication import services
-from gc_registry.core.database import cqrs, db
+from gc_registry.core.database import db
 
 # Router initialisation
 router = APIRouter(tags=["Accounts"])
 
 
 ### Account ###
-@router.post("/account", response_model=models.AccountRead)
+@router.post("/account", status_code=201)
 def create_account(
     account_base: models.AccountBase,
     # headers: dict = Depends(services.validate_user_and_get_headers),
     write_session: Session = Depends(db.db_name_to_client["db_write"].get_session),
     read_session: Session = Depends(db.db_name_to_client["db_read"].get_session),
 ):
-    print("Creating account: ", account_base)
-    db_account = models.Account.create(account_base, write_session, read_session)
+    models.Account.create(account_base, write_session, read_session)
 
-    return utils.format_json_response(
-        db_account, headers=None, response_model=models.AccountRead
-    )
+    return {"message": f"Account {account_base.account_name} created successfully."}
 
 
 @router.get("/account/{account_id}", response_model=models.AccountRead)
@@ -32,22 +29,22 @@ def read_account(
     # headers: dict = Depends(services.validate_user_and_get_headers),
     read_session: Session = Depends(db.db_name_to_client["db_read"].yield_session),
 ):
-    with next(db.db_name_to_client["db_write"].yield_session()) as write_session:
-        db_account = models.Account.by_id(account_id, write_session)
+    account = models.Account.by_id(account_id, read_session)
 
     return utils.format_json_response(
-        db_account, headers=None, response_model=models.AccountRead
+        account, headers=None, response_model=models.AccountRead
     )
 
 
-@router.patch("/account/{id}", response_model=models.AccountRead)
+@router.patch("/account/{account_id}", response_model=models.AccountRead)
 def update_account(
-    account: models.AccountRead,
+    account_id: int,
     account_update: models.AccountUpdate,
     # headers: dict = Depends(services.validate_user_and_get_headers),
-    write_session: Session = Depends(db.db_name_to_client["db_write"].yield_session),
-    read_session: Session = Depends(db.db_name_to_client["db_read"].yield_session),
+    write_session: Session = Depends(db.db_name_to_client["db_write"].get_session),
+    read_session: Session = Depends(db.db_name_to_client["db_read"].get_session),
 ):
+    account = models.Account.by_id(account_id, write_session)
     account_updated = account.update(account_update, write_session, read_session)
 
     return utils.format_json_response(
@@ -55,16 +52,14 @@ def update_account(
     )
 
 
-@router.delete("/account/{id}", response_model=models.AccountRead)
+@router.delete("/account/{account_id}", status_code=204)
 def delete_account(
     account_id: int,
     # headers: dict = Depends(services.validate_user_and_get_headers),
-    write_session: Session = Depends(db.db_name_to_client["db_write"].yield_session),
-    read_session: Session = Depends(db.db_name_to_client["db_read"].yield_session),
+    write_session: Session = Depends(db.db_name_to_client["db_write"].get_session),
+    read_session: Session = Depends(db.db_name_to_client["db_read"].get_session),
 ):
-    db_account = models.Account.by_id(account_id, read_session)
-    db_account = db_account.delete(write_session, read_session)
+    account = models.Account.by_id(account_id, write_session)
+    _account_deleted = account.delete(write_session, read_session)
 
-    return utils.format_json_response(
-        db_account, headers=None, response_model=models.AccountRead
-    )
+    return {"message": f"Account {account_id} deleted successfully."}
