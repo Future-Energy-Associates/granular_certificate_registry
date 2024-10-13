3f30852aa85a4dbe965d1dc2ad274bee13cf7342
Connor Galbraith <33663738+CAGalbraith@users.noreply.github.com>
2024-10-13 20:45:04 +0100

    Add `split_certificate_bundle` service

--- gc_registry/certificate/services.py
+++ gc_registry/certificate/services.py
@@ -1,5 +1,8 @@
 from hashlib import sha256
 
+from esdbclient import EventStoreDBClient
+from sqlalchemy.orm import Session
+
 from .models import GranularCertificateBundle, GranularCertificateBundleBase
 
 
@@ -9,7 +12,7 @@ def validate_transfer():
 
 def create_bundle_hash(
     gc_bundle: GranularCertificateBundle,
-    nonce: str,
+    nonce: str = "",
 ):
     """
     Given a GC Bundle and a nonce taken from the hash of a parent bundle,
@@ -29,7 +32,7 @@ def create_bundle_hash(
     """
 
     return sha256(
-        f"{GranularCertificateBundleBase(**gc_bundle.model_dump_json())}{nonce}".encode()
+        f"{GranularCertificateBundleBase(**gc_bundle.model_dump())}{nonce}".encode()
     ).hexdigest()
 
 
@@ -53,3 +56,62 @@ def verifiy_bundle_lineage(
         create_bundle_hash(gc_bundle_child, gc_bundle_parent.hash)
         == gc_bundle_child.hash
     )
+
+
+def split_certificate_bundle(
+    gc_bundle: GranularCertificateBundle,
+    size_to_split: int,
+    write_session: Session,
+    read_session: Session,
+    esdb_client: EventStoreDBClient,
+):
+    """Given a GC Bundle, split it into two child bundles and return them.
+
+    Example operation: a parent bundle with 100 certificates, when passed a
+    size_to_split of 25, will return the first child bundle with 25 certificates
+    and the second bundle with 75. Each of these will be created separately as
+    new bundles, with the same issuance ID of the parent bundle, and the parent
+    bundle will be marked as deleted but preserved in the database for audit
+    and lineage purposes.
+
+    Args:
+        gc_bundle (GranularCertificateBundle): The parent GC Bundle
+        size_to_split (int): The number of certificates to split from
+            the parent bundle.
+
+    Returns:
+        tuple[GranularCertificateBundle, GranularCertificateBundle]: The two child GC Bundles
+    """
+
+    assert size_to_split > 0, "The size to split must be greater than 0"
+    assert (
+        size_to_split < gc_bundle.bundle_quantity
+    ), "The size to split must be less than the total certificates in the parent bundle"
+
+    # Create two child bundles
+    gc_bundle_child_1 = GranularCertificateBundleBase(**gc_bundle.model_dump())
+    gc_bundle_child_2 = GranularCertificateBundleBase(**gc_bundle.model_dump())
+
+    # Update the child bundles with the new quantities
+    gc_bundle_child_1.bundle_quantity = size_to_split
+    gc_bundle_child_1.bundle_id_range_end = (
+        gc_bundle_child_1.bundle_id_range_start + size_to_split
+    )
+    gc_bundle_child_1.hash = create_bundle_hash(gc_bundle_child_1, gc_bundle.hash)
+
+    gc_bundle_child_2.bundle_quantity = gc_bundle.bundle_quantity - size_to_split
+    gc_bundle_child_2.bundle_id_range_start = gc_bundle_child_1.bundle_id_range_end + 1
+    gc_bundle_child_2.hash = create_bundle_hash(gc_bundle_child_2, gc_bundle.hash)
+
+    # Mark the parent bundle as deleted
+    gc_bundle.delete(write_session, read_session, esdb_client)
+
+    # Write the child bundles to the database
+    db_gc_bundle_child_1 = GranularCertificateBundle.create(
+        gc_bundle_child_1, write_session, read_session, esdb_client
+    )
+    db_gc_bundle_child_2 = GranularCertificateBundle.create(
+        gc_bundle_child_2, write_session, read_session, esdb_client
+    )
+
+    return db_gc_bundle_child_1, db_gc_bundle_child_2
