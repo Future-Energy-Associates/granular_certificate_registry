adbb7aa98133f29f15b0ed06b6b8ed85dc18e932
Clem <clem@futureenergy.associates>
2024-10-15 16:51:32 +0100

    Certificate services and tests

--- gc_registry/certificate/services.py
+++ gc_registry/certificate/services.py
@@ -1,50 +1,81 @@
+import datetime
+import logging
+
+from esdbclient import EventStoreDBClient
+from fluent_validator import validate
+from sqlalchemy import func, select
 from sqlmodel import Session
+
 from gc_registry.certificate.models import GranularCertificateBundle
 from gc_registry.certificate.schemas import (
     CertificateStatus,
     GranularCertificateBundleBase,
 )
+from gc_registry.device.meter_data.abstract_client import MeterDataClient
 from gc_registry.device.services import (
     device_mw_capacity_to_wh_max,
+    get_all_devices,
     get_device_capacity_by_id,
 )
+from gc_registry.settings import settings
 
-from fluent_validator import validate
 
-
-def get_max_certificate_id_by_device_id(db_session: Session, device_id: int):
+def get_max_certificate_id_by_device_id(
+    db_session: Session, device_id: int
+) -> int | None:
     """Gets the maximum certificate ID from any bundle for a given device, excluding any withdrawn certificates
 
     Args:
+        db_session (Session): The database session
+        device_id (int): The device ID
+
+    Returns:
+        int: The maximum certificate ID
 
 
     """
 
-    stmt = select(max(GranularCertificateBundle.bundle_id_range_end)).where(
+    stmt = select(func.max(GranularCertificateBundle.bundle_id_range_end)).where(
         GranularCertificateBundle.device_id == device_id,
         GranularCertificateBundle.certificate_status != CertificateStatus.WITHDRAWN,
     )
 
     max_certificate_id = db_session.exec(stmt).first()
 
-    return int(max_certificate_id[0])
+    if not max_certificate_id[0]:
+        return None
+    else:
+        return int(max_certificate_id[0])
 
 
 def validate_granular_certificate_bundle(
-    gcb: GranularCertificateBundleBase, is_storage_device: bool
+    db_session: Session,
+    gcb: GranularCertificateBundleBase,
+    is_storage_device: bool,
+    hours: float = settings.CERTIFICATE_GRANULARITY_HOURS,
 ) -> None:
-
+    W_IN_MW = 1e6
     device_id = gcb.device_id
+    device_mw = get_device_capacity_by_id(db_session, device_id) / W_IN_MW
+    device_max_watts_hours = device_mw_capacity_to_wh_max(device_mw, hours)
 
-    device_mw = get_device_capacity_by_id(device_id)
-    device_max_watts_hours = device_mw_capacity_to_wh_max(device_mw)
-
-    device_max_certificate_id = get_max_certificate_id_by_device_id(device_id)
-
-    validate(gcb.face_value).less_than(device_max_watts_hours).equals(
-        gcb.bundle_id_range_end - gcb.bundle_id_range_start
+    device_max_certificate_id = get_max_certificate_id_by_device_id(
+        db_session, device_id
     )
-    validate(gcb.bundle_id_range_start).equals(device_max_certificate_id + 1)
+
+    if not device_max_certificate_id:
+        device_max_certificate_id = 0
+
+    # Validate the bundle face value is equal to the difference between the bundle ID range
+    # and less than the device max watts hours
+    validate(gcb.face_value, identifier="face_value").less_than(
+        device_max_watts_hours
+    ).equal(gcb.bundle_id_range_end - gcb.bundle_id_range_start)
+
+    # Validate the bundle ID range start is greater than the previous max certificate ID
+    validate(gcb.bundle_id_range_start, identifier="bundle_id_range_start").equal(
+        device_max_certificate_id + 1
+    )
 
     # At this point if integrating wtih EAC registry or possibility of cross registry transfer
     # add integrations with external sources for further validation e.g. cancellation of underlying EACs
@@ -56,11 +87,86 @@ def validate_granular_certificate_bundle(
     return None
 
 
-def create_granular_certificate_bundle(
-    db_session: Session, gcb: GranularCertificateBundleBase
-):
+def issue_certificates_in_date_range(
+    from_datetime: datetime.datetime,
+    to_datetime: datetime.datetime,
+    db_write_engine: Session,
+    db_read_engine: Session,
+    esdb_client: EventStoreDBClient,
+    issuance_metadata_id: int,
+    meter_data_client: MeterDataClient,
+) -> list[GranularCertificateBundle]:
+    """Issues certificates for a device using the following process.
+    1. Get a list of devices in the registry and their capacities
+    2. For each device, get the meter data for the device for the given period
+    3. Map the meter data to certificates
+    4. Validate the certificates
+    5. Commit the certificates to the database
 
-    db_session.add(bundle)
-    db_session.commit()
-    db_session.refresh(bundle)
-    return bundle
+    Args:
+        from_datetime (datetime.datetime): The start of the period
+        to_datetime (datetime.datetime): The end of the period
+        db_write_engine (Session): The database write session
+        db_read_engine (Session): The database read session
+        issuance_metadata_id (int): The issuance metadata ID
+        meter_data_client (MeterDataClient, optional): The meter data client. Defaults to Depends(ElexonClient).
+
+    Returns:
+        list[GranularCertificateBundle]: The list of certificates issued
+
+    """
+
+    # Get the devices in the registry
+    devices = [d[0] for d in get_all_devices(db_read_engine)]
+
+    # Issue certificates for each device
+    certificates = []
+    for device in devices:
+        # Get the meter data for the device
+        if not device.meter_data_id:
+            logging.error(f"No meter data ID for device: {device.id}")
+            continue
+
+        meter_data = meter_data_client.get_generation_by_device_in_datetime_range(
+            from_datetime, to_datetime, device.meter_data_id
+        )
+
+        if not meter_data:
+            logging.info(f"No meter data retrieved for device: {device.meter_data_id}")
+            continue
+
+        # Map the meter data to certificates
+        bundle_id_range_start = get_max_certificate_id_by_device_id(
+            db_read_engine, device.id
+        )
+        if not bundle_id_range_start:
+            bundle_id_range_start = 1
+        else:
+            bundle_id_range_start += 1
+
+        certificates = meter_data_client.map_generation_to_certificates(
+            meter_data,
+            bundle_id_range_start,
+            device.account_id,
+            device.id,
+            device.is_storage,
+            issuance_metadata_id,
+        )
+
+        if not certificates:
+            logging.info(f"No meter data retrieved for device: {device.meter_data_id}")
+            continue
+
+        # Validate the certificates
+        for certificate in certificates:
+            validate_granular_certificate_bundle(
+                db_read_engine, certificate, is_storage_device=device.is_storage
+            )
+
+            # Commit the certificate to the database
+            # TODO: Consider using bulk transaction - will require change in validation of bundle_id_range_start and end
+            created_entities = GranularCertificateBundle.create(
+                certificate, db_write_engine, db_read_engine, esdb_client
+            )
+
+    return created_entities
--- gc_registry/tests/certificate/test_certificate_services.py
+++ gc_registry/tests/certificate/test_certificate_services.py
@@ -1,14 +1,138 @@
-from gc_registry.certificate.services import get_max_certificate_id_by_device_id
+import datetime
+
+import pytest
+
+from gc_registry.certificate.schemas import GranularCertificateBundleBase
+from gc_registry.certificate.services import (
+    get_max_certificate_id_by_device_id,
+    issue_certificates_in_date_range,
+    validate_granular_certificate_bundle,
+)
+from gc_registry.device.meter_data.elexon.elexon import ElexonClient
+from gc_registry.device.models import Device
+from gc_registry.settings import settings
 
 
 def test_get_max_certificate_id_by_device_id(
-    db_session,
+    db_read_session,
     fake_db_wind_device,
     fake_db_solar_device,
-    fake_db_granular_certificate_bundle,
+    fake_db_gc_bundle,
 ):
-
     max_certificate_id = get_max_certificate_id_by_device_id(
-        db_session, fake_db_wind_device.id
+        db_read_session, fake_db_wind_device.id
     )
-    assert max_certificate_id == fake_db_granular_certificate_bundle.bundle_id_range_end
+    assert max_certificate_id == fake_db_gc_bundle.bundle_id_range_end
+
+
+def test_get_max_certificate_id_by_device_id_no_certificates(
+    db_read_session,
+    fake_db_wind_device,
+):
+    max_certificate_id = get_max_certificate_id_by_device_id(
+        db_read_session, fake_db_wind_device.id
+    )
+    assert max_certificate_id is None
+
+
+def test_validate_granular_certificate_bundle(
+    db_read_session,
+    fake_db_wind_device,
+    fake_db_solar_device,
+    fake_db_gc_bundle,
+):
+    hours = settings.CERTIFICATE_GRANULARITY_HOURS
+
+    # Test case 1: certificate already exists for the device in the given period
+    # This will fail because the bundle_id_range_start is not equal to the max_certificate_id + 1
+    gcb_dict = fake_db_gc_bundle.model_dump()
+    fake_db_gc_bundle_base = GranularCertificateBundleBase.model_validate(gcb_dict)
+    with pytest.raises(ValueError) as exc_info:
+        validate_granular_certificate_bundle(
+            db_read_session, fake_db_gc_bundle_base, is_storage_device=False
+        )
+    assert "bundle_id_range_start does not match criteria for equal" in str(
+        exc_info.value
+    )
+
+    # Lets update the bundle_id_range_start to be equal to the max_certificate_id + 1,
+    # the face_value and bundle_id_range_end to be equal to the difference between the bundle ID range
+    fake_db_gc_bundle_base.bundle_id_range_start = (
+        fake_db_gc_bundle.bundle_id_range_end + 1
+    )
+    fake_db_gc_bundle_base.bundle_id_range_end = (
+        fake_db_gc_bundle_base.bundle_id_range_start + fake_db_gc_bundle_base.face_value
+    )
+
+    validate_granular_certificate_bundle(
+        db_read_session, fake_db_gc_bundle_base, is_storage_device=False
+    )
+
+    # Test case 2: certificate face value is greater than the device max watts hours
+    # This will fail because the face_value is greater than the device max watts hours
+
+    fake_db_gc_bundle_base.face_value = (fake_db_wind_device.capacity * hours) + 1
+    fake_db_gc_bundle_base.bundle_id_range_end = (
+        fake_db_gc_bundle_base.bundle_id_range_start + fake_db_gc_bundle_base.face_value
+    )
+
+    with pytest.raises(ValueError) as exc_info:
+        validate_granular_certificate_bundle(
+            db_read_session, fake_db_gc_bundle_base, is_storage_device=False
+        )
+    assert "face_value does not match criteria for less_than" in str(exc_info.value)
+
+    fake_db_gc_bundle_base.face_value = (fake_db_wind_device.capacity * hours) - 1
+    fake_db_gc_bundle_base.bundle_id_range_end = (
+        fake_db_gc_bundle_base.bundle_id_range_start + fake_db_gc_bundle_base.face_value
+    )
+
+    validate_granular_certificate_bundle(
+        db_read_session, fake_db_gc_bundle_base, is_storage_device=False
+    )
+
+
+def test_issue_certificates_in_date_range(
+    db_write_session,
+    db_read_session,
+    fake_db_account,
+    fake_db_issuance_metadata,
+    esdb_client,
+):
+    from_datetime = datetime.datetime(2024, 1, 1, 0, 0, 0)
+    to_datetime = from_datetime + datetime.timedelta(hours=2)
+    meter_data_id = "T_RATS-4"
+
+    client = ElexonClient()
+
+    device_capacities = client.get_device_capacities([meter_data_id])
+
+    # create a new device
+    device_dict = {
+        "device_name": "Ratcliffe on Soar",
+        "meter_data_id": meter_data_id,
+        "grid": "National Grid",
+        "energy_source": "wind",
+        "technology_type": "wind",
+        "operational_date": str(datetime.datetime(2015, 1, 1, 0, 0, 0)),
+        "capacity": device_capacities[meter_data_id],
+        "peak_demand": 100,
+        "location": "Some Location",
+        "account_id": fake_db_account.id,
+        "is_storage": False,
+    }
+    device = Device.create(device_dict, db_write_session, db_read_session, esdb_client)
+
+    assert device is not None
+
+    issued_certificates = issue_certificates_in_date_range(
+        from_datetime,
+        to_datetime,
+        db_write_session,
+        db_read_session,
+        esdb_client,
+        fake_db_issuance_metadata.id,
+        client,
+    )
+
+    assert issued_certificates is not None
