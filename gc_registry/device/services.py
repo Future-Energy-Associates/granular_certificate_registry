54f6510995f9b8396a1773b27c2e6f78196634d7
Clem <clem@futureenergy.associates>
2024-10-15 16:51:52 +0100

    Device services and tests

--- gc_registry/device/services.py
+++ gc_registry/device/services.py
@@ -1,8 +1,15 @@
 from sqlalchemy import select
 from sqlmodel import Session
-from gc_registry.certificate.models import GranularCertificateBundle
 
 from gc_registry.device.models import Device
+from gc_registry.settings import settings
+
+
+def get_all_devices(db_session: Session) -> list[Device]:
+    stmt = select(Device)
+    devices = db_session.exec(stmt).all()
+
+    return list(devices)
 
 
 def get_device_capacity_by_id(db_session: Session, device_id: int) -> float:
@@ -13,7 +20,10 @@ def get_device_capacity_by_id(db_session: Session, device_id: int) -> float:
     return float(device_capacity[0])
 
 
-def device_mw_capacity_to_wh_max(device_capacity_mwh: float, hours: float = 0.5):
+def device_mw_capacity_to_wh_max(
+    device_capacity_mw: float, hours: float = settings.CERTIFICATE_GRANULARITY_HOURS
+) -> float:
     """Take the device capacity in MW and calculate the maximum Watt-Hours
     the device can produce in a given number of hours"""
-    return device_capacity_mwh * 1000 * hours
+    WH_IN_MW = 1e6
+    return device_capacity_mw * WH_IN_MW * hours
--- gc_registry/tests/device/test_device_services.py
+++ gc_registry/tests/device/test_device_services.py
@@ -1,7 +1,18 @@
-from gc_registry.device.services import get_device_capacity_by_id
+from gc_registry.device.services import get_all_devices, get_device_capacity_by_id
 
 
-def test_get_device_capacity_by_id(db_session, fake_db_wind_device) -> None:
+def test_get_device_capacity_by_id(db_read_session, fake_db_wind_device) -> None:
+    device_capacity = get_device_capacity_by_id(db_read_session, fake_db_wind_device.id)
+    assert round(device_capacity, 1) == round(fake_db_wind_device.capacity, 1)
 
-    device_capacity = get_device_capacity_by_id(db_session, fake_db_wind_device.id)
-    assert round(device_capacity, 1) == round(3000, 1)
+
+def test_get_all_devices(
+    db_read_session, fake_db_wind_device, fake_db_solar_device
+) -> None:
+    devices = get_all_devices(db_read_session)
+    devices = [d[0] for d in devices]
+    assert len(devices) == 2
+    assert devices[0].id == fake_db_wind_device.id
+    assert devices[1].id == fake_db_solar_device.id
+    assert devices[0].capacity == fake_db_wind_device.capacity
+    assert devices[1].capacity == fake_db_solar_device.capacity
