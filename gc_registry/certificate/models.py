d07a5d44a8256a86b4609575bbb130e5acd502e1
Connor Galbraith <33663738+CAGalbraith@users.noreply.github.com>
2024-10-30 12:19:49 +0000

    Add certificate update model

--- gc_registry/certificate/models.py
+++ gc_registry/certificate/models.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel
 from sqlalchemy import Column, Integer
 from sqlmodel import Field
 
@@ -7,6 +8,7 @@ from gc_registry.certificate.schemas import (
     GranularCertificateBundleBase,
     IssuanceMetaDataBase,
 )
+from gc_registry.core.models.base import CertificateStatus
 
 # issuance_id a unique non-sequential ID related to the issuance of the entire bundle,
 # specified as a concatenation of deviceID-EnergyCarrier-ProductionStartDatetime.
@@ -23,6 +25,15 @@ class GranularCertificateBundle(
         primary_key=True,
         description="A unique, incremental integer ID assigned to this bundle.",
     )
+
+
+class GranularCertificateBundleUpdate(BaseModel):
+    account_id: int | None = None
+    certificate_status: CertificateStatus | None = None
+    metadata_id: int | None = None
+    bundle_id_range_start: int | None = None
+    bundle_id_range_end: int | None = None
+    bundle_quantity: int | None = None
 
 
 # A Transfer object is specified by a User, and is stored in a transaction table that
