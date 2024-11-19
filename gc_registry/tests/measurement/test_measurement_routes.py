92e6e5337281c03fb73c67dbad21a902e3e87295
Connor Galbraith <33663738+CAGalbraith@users.noreply.github.com>
2024-11-19 15:01:00 +0000

    Update measurement route test

--- gc_registry/tests/measurement/test_measurement_routes.py
+++ gc_registry/tests/measurement/test_measurement_routes.py
@@ -1,29 +1,53 @@
-
-
+import json
 from unittest.mock import MagicMock
+
 import pandas as pd
 import pytest
 
 
 @pytest.fixture
 def valid_measurement_json():
+    return json.dumps(
+        [
+            {
+                "device_id": 1,
+                "interval_usage": 10,
+                "interval_start_datetime": "2024-11-18T10:00:00",
+                "interval_end_datetime": "2024-11-18T11:00:00",
+                "gross_net_indicator": "NET",
+            },
+            {
+                "device_id": 1,
+                "interval_usage": 15,
+                "interval_start_datetime": "2024-11-18T11:00:00",
+                "interval_end_datetime": "2024-11-18T12:00:00",
+                "gross_net_indicator": "NET",
+            },
+            {
+                "device_id": 2,
+                "interval_usage": 20,
+                "interval_start_datetime": "2024-11-18T10:00:00",
+                "interval_end_datetime": "2024-11-18T11:00:00",
+                "gross_net_indicator": "NET",
+            },
+        ]
+    )
 
-    return """
-    [
-        {"device_id": "device_1", "interval_usage": 10, "interval_start_datetime": "2024-11-18T10:00:00", "interval_end_datetime": "2024-11-18T11:00:00"},
-        {"device_id": "device_1", "interval_usage": 15, "interval_start_datetime": "2024-11-18T11:00:00", "interval_end_datetime": "2024-11-18T12:00:00"},
-        {"device_id": "device_2", "interval_usage": 20, "interval_start_datetime": "2024-11-18T10:00:00", "interval_end_datetime": "2024-11-18T11:00:00"}
-    ]
-    """
 
-
-def test_submit_readings_success(api_client, db_write_session, db_read_session, esdb_client, valid_measurement_json, parsed_measurement_df, monkeypatch
+def test_submit_readings_success(
+    api_client,
+    db_write_session,
+    db_read_session,
+    esdb_client,
+    valid_measurement_json,
+    parsed_measurement_df,
+    monkeypatch,
 ):
     """Test successful submission of readings."""
 
     response = api_client.post(
         "measurement/submit_readings/",
-        json={"measurement_json": valid_measurement_json},
+        params={"measurement_json": valid_measurement_json},
     )
 
     print(response.json())
@@ -32,6 +56,6 @@ def test_submit_readings_success(api_client, db_write_session, db_read_session, 
 
     response_data = response.json()
     assert response_data["message"] == "Readings submitted successfully."
-    assert response_data["total_usage_per_device"] == {"device_1": 25, "device_2": 20}
+    assert response_data["total_usage_per_device"] == {1: 25, 2: 20}
     assert response_data["first_reading_datetime"] == "2024-11-18T10:00:00"
     assert response_data["last_reading_datetime"] == "2024-11-18T12:00:00"
