a0f18c41f8d549d5bfb251d96c8801f2cf5b28b5
Clem <clem@futureenergy.associates>
2025-06-09 21:42:37 +0100

    Merge changes

--- gc_registry/tests/storage/test_storage_routes.py
+++ gc_registry/tests/storage/test_storage_routes.py
@@ -2,10 +2,8 @@ import io
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlmodel import Session, select
-from sqlmodel.sql.expression import SelectOfScalar
+from sqlmodel import Session
 
-from gc_registry.certificate.models import GranularCertificateBundle, IssuanceMetaData
 from gc_registry.device.models import Device
 
 
@@ -78,8 +76,6 @@ def test_submit_storage_records_success(
         data=data,
         headers={"Authorization": f"Bearer {token_storage_validator}"},
     )
-
-    print(response.text)
 
     assert response.status_code == 201
     response_data = response.json()
